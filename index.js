52b35619f2ea32439f0f12656fa846c1f3b49575
Piripe <jeuxdepierre@laposte.net>
2023-01-27 17:42:31 +0100

    Implemented CLI.

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const {} = require("./src/data/prototype/User");
 const {} = require("./src/xp/prototype/User");
-const {} = require('./src/inventory/prototype/User');
-const {} = require('./src/inventory/shop/prototype/User');
+const {} = require("./src/inventory/prototype/User");
+const {} = require("./src/inventory/shop/prototype/User");
 const { init } = require("./src/client");
 const {} = require("./src/events");
 const db = require("./src/data/db");
 
-init({ test: true, resetCommands:false });
-db.update();
+const config = {
+    test: false,
+    resetCommands: false,
+};
+const args = process.argv.slice(2);
+
+let noStart = false;
+
+args.forEach((arg, i) => {
+    switch (arg) {
+        case "-t":
+        case "--test":
+            config.test = true;
+            break;
+        case "-r":
+        case "--reset-commands":
+            config.resetCommands = true;
+            break;
+        case "-h":
+        case "--help":
+            console.log(`Usage: node index [arguments]
+            
+Options:
+-t, --test              Run the bot in test mode.
+-r, --reset-commands    Reset bot's commands.`);
+            noStart=true;
+            break;
+    }
+});
+
+if (!noStart) {
+    init(config);
+    db.update();
+}
